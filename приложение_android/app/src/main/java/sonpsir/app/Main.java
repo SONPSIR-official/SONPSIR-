a3cdfa01c0cb308c1294bc6b21580c3b03a89ac7
SONPSIR-official <SONPSIR-official@users.noreply.github.com>
2026-08-10 14:03:44 +0300

    Чистка репозитория: удаление временных файлов и баз данных

--- приложение_android/app/src/main/java/sonpsir/app/Main.java
+++
@@ -1,3 +0,0 @@
-package sonpsir.app;
-import android.app.Activity;import android.os.Bundle;import android.webkit.WebView;
-public class Main extends Activity{protected void onCreate(Bundle s){super.onCreate(s);WebView w=new WebView(this);w.getSettings().setJavaScriptEnabled(true);w.loadUrl("http://10.0.2.2:8080");setContentView(w);}}
--- программа.sps
+++
@@ -1,2 +0,0 @@
-#!/data/data/com.termux/files/usr/bin/env sonpsir
-что печатать("Я скрипт SONPSIR — запускаюсь как ./программа.sps")
